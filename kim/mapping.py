981b2e56e8f47de5756db7d81e42e4f881cbb75f
Mike Waites <mikey.waites@gmail.com>
2014-03-24 19:05:48 +0000

    Handle duplicate only/exclude fields passed to a mapping

--- kim/mapping.py
+++ kim/mapping.py
@@ -1,5 +1,7 @@
 from collections import OrderedDict
 
+from .exceptions import MappingError
+
 
 class Mapping(object):
     """:class:`kim.mapping.Mapping` is a factory for generating data
--- kim/tests/test_mapping.py
+++ kim/tests/test_mapping.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-from kim.mapping import Mapping
+from kim.mapping import Mapping, MappingError
 
 
 class FieldInterface(object):
@@ -52,3 +52,30 @@ class MappingTest(unittest.TestCase):
         exp = Mapping.collection()
         exp['field_a'] = field_b
         self.assertDictEqual(exp, mapping.mapped())
+
+    def test_field_ignored_when_only_is_provided(self):
+
+        field_a = FieldInterface()
+        field_b = FieldInterface()
+
+        initial = {'field_a': field_a}
+        mapping = Mapping(only=['field_b'], **initial)
+        mapping.add('field_b', field_b)
+        exp = Mapping.collection()
+        exp['field_b'] = field_b
+
+        self.assertNotIn('field_a', mapping.mapped())
+
+    def test_field_rejected_when_in_exclude(self):
+
+        field_a = FieldInterface()
+
+        initial = {'field_a': field_a}
+        mapping = Mapping(exclude=['field_a'], **initial)
+        exp = Mapping.collection()
+        self.assertDictEqual(exp, mapping.mapped())
+
+    def test_mapping_error_raised_when_duplicates_in_only_excluded(self):
+
+        with self.assertRaises(MappingError):
+            Mapping(only=['foo'], exclude=['foo', 'bar'])
