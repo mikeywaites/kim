f63090859fadaecc8054f7220d411ebbafb2858c
Mike Waites <mikey.waites@gmail.com>
2014-03-24 17:45:15 +0000

    More tests for mapping.Mapping

--- kim/tests/test_mapping.py
+++ kim/tests/test_mapping.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import pytest
 
 from kim.mapping import Mapping
 
@@ -17,13 +16,39 @@ class MappingTest(unittest.TestCase):
 
     def test_add_initial_mapping(self):
 
-        self.maxDiff = None
         field_a = FieldInterface()
-        field_b = FieldInterface()
 
         initial = {'field_a': field_a}
         mapping = Mapping(**initial)
 
         exp = Mapping.collection()
         exp['field_a'] = field_a
+        self.assertDictEqual(exp, mapping.mapped())
+
+    def test_mapping_with_no_initial_mapping(self):
+
+        mapping = Mapping()
+        self.assertDictEqual(Mapping.collection(), mapping.mapped())
+
+    def test_add_property_to_mapping(self):
+
+        field_a = FieldInterface()
+        mapping = Mapping()
+        mapping.add('field_a', field_a)
+
+        exp = Mapping.collection()
+        exp['field_a'] = field_a
+
+        self.assertDictEqual(exp, mapping.mapped())
+
+    def test_add_property_overwrites_initial_property(self):
+        field_a = FieldInterface()
+        field_b = FieldInterface()
+
+        initial = {'field_a': field_a}
+        mapping = Mapping(**initial)
+        mapping.add('field_a', field_b)
+
+        exp = Mapping.collection()
+        exp['field_a'] = field_b
         self.assertDictEqual(exp, mapping.mapped())
