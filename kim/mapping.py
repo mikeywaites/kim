50bc0cdb4054bff1901a972d5b2f8b142ab8c692
Mike Waites <mikey.waites@gmail.com>
2014-03-24 19:06:27 +0000

    More documentation for mapping methods.

--- kim/mapping.py
+++ kim/mapping.py
@@ -47,24 +47,83 @@ class Mapping(object):
     >>> mapping.mapped()
     >>> OrderedDict([('field_a', <field String>)])
 
+    Limiting the fields added to a mapping
+    --------------------------------------
+    When mappings are declared you may optionally limit the fields added
+    by using the `only` and `exclude` params passed the a mapping
+    instance.
+
+    >>> mapping = Mapping(only=['field_a'])
+    >>> mapping = Mapping(exclude=['field_b'])
+
+    Its prefered to only specify one of the parameters at a time.
+    The fields defined must be unqiue in each option or a
+    :class:`kim.exceptions.MappingError` exception will be raised.
+
     """
 
     collection = OrderedDict
 
-    def __init__(self, **mappings):
+    def __init__(self, only=None, exclude=None, **mappings):
         """:class:`kim.mapping.Mapping` constructor.
 
         :param mapping: field name and field key value pairs.
+        :param only: specify an iterable of field_names that may ONLY
+                     appear in the mapping.
+        :param exclude: specify an iterable of field_names that may MUST
+                        not appear in the mapping
 
+        :raises: :class:`kim.exceptions.MappingError`
         """
+        self.only = only or list()
+        self.only = set(self.only)
+
+        self.exclude = exclude or list()
+        self.exclude = set(self.exclude)
+
+        if any(self.only & self.exclude):
+            raise MappingError('only and exclude mappings '
+                               'must not contain duplicate fields')
+
         self._mapping = self.collection()
         for field_name, field in mappings.iteritems():
             self.add(field_name, field)
 
     def mapped(self):
+        """Returns the dict like object containing all the
+        properties defined in this mapping.
 
+        :rtype: `self.collection`
+        :returns: collection of mapped properties
+        """
         return self._mapping
 
-    def add(self, field_name, field):
+    def _add_mapping(self, field_name, field):
+        """Stores a mapping by field_name: field in the mapping
+        collection class
 
+        :param field_name str: named of this mapped property
+        :param field: obj implementing the field interface
+
+        :raises: None
+        :returns: None
+
+        """
         self._mapping[field_name] = field
+
+    def add(self, field_name, field):
+        """Add a new property to mapping ensuring field_name is
+        permitted inside of this mapping.
+
+        :param field_name str: named of this mapped property
+        :param field: obj implementing the field interface
+
+        :raises: None
+        :returns: None
+        """
+
+        if ((self.only and field_name not in self.only) or
+                (self.exclude and field_name in self.exclude)):
+            return
+
+        self._add_mapping(field_name, field)
