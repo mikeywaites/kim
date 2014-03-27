ca1256f83add58793ba0d429921714e2070f7fc5
Mike Waites <mikey.waites@gmail.com>
2014-03-27 15:47:57 +0000

    Moved NestedType to types module

--- kim/types.py
+++ kim/types.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from .serializers import SerializerABC
+from .mapping import MappingABC
+
 
 class TypeABC(object):
 
@@ -15,3 +18,36 @@ class String(TypeABC):
 
 class Integer(TypeABC):
     pass
+
+
+class Nested(TypeABC):
+
+    def __init__(self, name='', mapped=None,
+                 nullable=True, role=None, *args, **kwargs):
+
+        self._mapping = None
+        self.mapping = mapped
+        self.nullable = True
+        self.role = role
+
+        super(Nested, self).__init__(name, *args, **kwargs)
+
+    @property
+    def mapping(self):
+        return self._mapping
+
+    @mapping.setter
+    def mapping(self, mapped):
+        if isinstance(mapped, MappingABC):
+            self._mapping = mapped
+        elif isinstance(mapped, SerializerABC):
+            self._mapping = mapped.__mapping__
+        else:
+            raise TypeError('Nested() must be called with a '
+                            'mapping or a mapped serializer instance')
+
+    def get_fields(self):
+        if self.role:
+            return self.role.get_mapping(self.mapping).fields
+
+        return self.mapping.fields
