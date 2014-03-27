fc2723ea819755da11a991c32ca247a60388bb49
Mike Waites <mikey.waites@gmail.com>
2014-03-27 16:25:04 +0000

    docs for roles module and added some helper utlilities.

--- kim/roles.py
+++ kim/roles.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """ The `Roles` module provides the core system for creating and using
-`Roles` system with KIM.
+the `Roles` system with KIM.
 
 """
 
 
 class RoleABC(object):
     """Role Abstract Base Class.  All `Role` types MUST inherit from this
-    class
+    base class.
     """
-
-    def __init__(self, name, *field_names, **kwargs):
-        self.name = name
-        self.field_names = field_names
+    pass
 
 
 class Role(RoleABC):
@@ -22,25 +19,124 @@ class Role(RoleABC):
     used in a Mapping or Mapped Serializer.  They allow users
     to specify custom sets of field names that are permitted
     or disabled for a defined mapping.
+
+    :param name: The name given to this Role e.g::
+        Role('my_role')
+
+    :param field_names: The field names for this role.  Any number of field
+    names may be specified as positional arguments
+    to the :class:`Role` constructor e.g::
+        Role('my_role', 'field_1', 'field_2)
+
+    :param whitelist: Specify wether the `field_names` defined should be allowed
+    or disabled for this role. (see :meth:`membership`)
     """
 
     def __init__(self, name, *field_names, **kwargs):
         super(Role, self).__init__(name, *field_names, **kwargs)
+        self.name = name
+        self.field_names = field_names
         self.whitelist = kwargs.pop('whitelist', True)
 
     def membership(self, field_name):
+        """Returns a boolean indicating the membership for `field_name`
+        in this `role`
+
+        For whitelisted Roles, the :meth:`membership` will check to see
+        if `field_name` is defined in `self.field_names`. For blacklisted
+        (whitelist=False) the inverse is checked
+
+        This method may be overriden to provide more complex membership
+        tests if required.
+
+        e.g::
+            >>>role = Role('my_role', 'name', 'email')
+            >>>role.membership('foo')
+            >>>False
+
+        :rtype: bool
+        :returns: True if the field_name is permitted in this role.
+        """
         if self.whitelist:
             return field_name in self.field_names
         else:
             return field_name not in self.field_names
 
     def get_mapping(self, mapping):
+        """Creates a new mapping based on this role.
 
-        fields = [field for field in mapping.fields
-                  if self.membership(field.name)]
+        :param mapping: a :class:`kim.mapping.MappingABC` mapping type.
 
-        MappingKlass = mapping.__class__
-        return MappingKlass(
-            mapping.name,
-            *fields
-        )
+        :rtype: :class:`kim.mapping.MappingABC`
+        :returns: a new mapping from this role
+
+        .. seealso::
+            :func:`create_mapping_from_role`
+        """
+
+        return create_mapping_from_role(self, mapping)
+
+
+def create_mapping_from_role(role, mapping):
+    """utility method that creates a new mapping from `mapping`
+    pulling in the allowed fields defined in `role.field_names`
+
+    :param role: :class:`RoleABC` type
+    :param mapping: :class:`kim.mapping.MappingABC` type
+
+    :rtype: :class:`kim.mapping.MappingABC`
+    :returns: New mapping constructred from role.field_names
+    """
+    fields = [field for field in mapping.fields
+              if role.membership(field.name)]
+
+    # We create a new class here from mapping to allow custom mappers
+    # TODO what other args, kwargs can a mapping have?
+    MappingKlass = mapping.__class__
+    return MappingKlass(
+        mapping.name,
+        *fields
+    )
+
+
+def whitelist(name, fields, role_base=None, **kwargs):
+    """Helper function that explicitly creates a new :class`Role` type
+    setting the whitelist option to True
+
+    :param name: the name given to this role
+    :param fields: iterable of field names
+
+    e.g::
+        role=whitelist('user_public', ['name', 'email'])
+
+    :returns: New :class:`Role` type
+    """
+
+    BaseKlass = role_base or Role
+    return BaseKlass(
+        name,
+        whitelist=True,
+        *fields,
+        **kwargs
+    )
+
+
+def blacklist(name, fields, role_base=None, **kwargs):
+    """Helper function that explicitly creates a new :class`Role` type
+    setting the whitelist option to False
+
+    :param name: the name given to this role
+    :param fields: iterable of field names
+
+    e.g::
+        role=blacklist('user_public', ['name', 'email'])
+
+    :returns: New :class:`Role` type
+    """
+    BaseKlass = role_base or Role
+    return BaseKlass(
+        name,
+        whitelist=False,
+        *fields,
+        **kwargs
+    )
