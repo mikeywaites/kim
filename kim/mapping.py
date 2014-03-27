b4879c514ff94bce084b9390954fd5cd1be62724
Mike Waites <mikey.waites@gmail.com>
2014-03-27 15:18:25 +0000

    Rough Roles implementation

--- kim/mapping.py
+++ kim/mapping.py
@@ -8,6 +8,44 @@ class FieldABC(object):
         self.source = source
 
 
+class SerializerABC(object):
+
+    __mapping__ = None
+
+
+class Nested(FieldABC):
+
+    def __init__(self, name='', mapped=None,
+                 nullable=True, role=None, *args, **kwargs):
+
+        self._mapping = None
+        self.mapping = mapped
+        self.nullable = True
+        self.role = role
+
+        super(Nested, self).__init__(name, *args, **kwargs)
+
+    @property
+    def mapping(self):
+        return self._mapping
+
+    @mapping.setter
+    def mapping(self, mapped):
+        if isinstance(mapped, Mapping):
+            self._mapping = mapped
+        elif isinstance(mapped, SerializerABC):
+            self._mapping = mapped.__mapping__
+        else:
+            raise TypeError('Nested() must be called with a '
+                            'mapping or a mapped serializer instance')
+
+    def get_fields(self):
+        if self.role:
+            return self.role.get_mapping(self.mapping).fields
+
+        return self.mapping.fields
+
+
 class String(FieldABC):
     pass
 
@@ -18,24 +56,43 @@ class Integer(FieldABC):
 
 class RoleABC(object):
 
-    def __init__(self, name='', *fields):
+    def __init__(self, name, *field_names, **kwargs):
         self.name = name
-        self.fields = [].extend(fields)
+        self.field_names = field_names
 
 
 class Role(RoleABC):
-    pass
+
+    def __init__(self, name, *field_names, **kwargs):
+        super(Role, self).__init__(name, *field_names, **kwargs)
+        self.whitelist = kwargs.pop('whitelist', True)
+
+    def membership(self, field_name):
+        if self.whitelist:
+            return field_name in self.field_names
+        else:
+            return field_name not in self.field_names
+
+    def get_mapping(self, mapping):
+
+        fields = [field for field in mapping.fields
+                  if self.membership(field.name)]
+
+        MappingKlass = mapping.__class__
+        return MappingKlass(
+            mapping.name,
+            *fields
+        )
 
 
 class Mapping(object):
     """:class:`kim.mapping.Mapping` is a factory for generating data
     structures in KIM.
 
-    A mapping consists of a collection of kim `field` interfaces and
-    roles.
+    A mapping consists of a collection of kim `field` types.
 
-    `Mappings` are created by passing `Fields` and `Roles`
-    to the contructor of :class:`kim.mapping.Mapping`
+    `Mappings` are created by passing `Fields` to the
+    contructor of :class:`kim.mapping.Mapping`
 
      e.g.::
 
@@ -43,12 +100,10 @@ class Mapping(object):
             'my_mapping',
              String('name'),
              Integer('id'),
-             Role('overview', 'name', 'id')
         )
 
     The first argument to the Mapping type is the name of this mapping,
-    the following arguments may be any mixture of `Field` and `Role`
-    types
+    the following arguments may be any mixture of `Field` types.
 
     :param name: The user defined name of this `mapping`
 
@@ -63,7 +118,6 @@ class Mapping(object):
     .. seealso::
 
         :class:`kim.fields.FieldABC`
-        :class:`kim.roles.RoleABC`
 
     """
 
@@ -78,7 +132,6 @@ class Mapping(object):
 
         self.name = mapping_name
         self.fields = kwargs.get('collection', list())
-        self.roles = {}
 
         self._arg_loop(args)
 
@@ -98,21 +151,6 @@ class Mapping(object):
             if isinstance(item, FieldABC):
                 self.add_field(item)
 
-            if isinstance(item, RoleABC):
-                self.add_role(item)
-
-    def add_role(self, role):
-        """Adds a role to the roles dict keyed by `role.name`: `role`
-
-        :param role: a `Role` type
-
-        .. seealso::
-            :class:`kim.roles.RoleABC`
-
-        :returns: None
-        """
-        self.roles.update({role.name: role})
-
     def add_field(self, field):
         """Add a `field` type to the `fields` collection.
 
@@ -124,3 +162,18 @@ class Mapping(object):
         :returns: None
         """
         self.fields.append(field)
+
+
+if __name__=="__main__":
+    other_mapping = Mapping(
+        'food',
+        String('name'),
+        String('name_2'),
+    )
+    mapping = Mapping(
+        'users',
+        String('name'),
+        Nested('food', other_mapping, role=Role('test', 'name')),
+    )
+    mapping.fields[1].get_fields()
+    print 'foo'
