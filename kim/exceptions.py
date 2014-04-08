7f3331648c594a338a0e983562be9b6bcec1901c
Mike Waites <mikey.waites@gmail.com>
2014-04-08 12:21:39 +0100

    adding new MappingIterator system.

--- kim/exceptions.py
+++ kim/exceptions.py
@@ -8,9 +8,18 @@ class MappingError(KimError):
     pass
 
 
+#TODO REMOVE THIS
 class ValidationError(KimError):
     pass
 
 
+class ValidationErrors(KimError):
+    pass
+
+
 class RoleNotFound(KimError):
+    pass
+
+
+class FieldError(KimError):
     pass
