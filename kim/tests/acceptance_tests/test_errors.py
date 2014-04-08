e20176acd52e8354aedf4a946d91117c33bffe17
Jack Saunders <jack@believe.in>
2014-04-08 11:16:51 +0000

    warn people if they configure their Serializers incorrectly

---
+++ kim/tests/acceptance_tests/test_errors.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import date, datetime
+
+from iso8601.iso8601 import Utc
+
+from kim.serializers import Serializer, Field
+from kim.types import (String, Collection, Nested, Integer, Email, Date,
+    DateTime)
+from kim.exceptions import ConfigurationError
+
+
+class ErrorHandlingAcceptanceTests(unittest.TestCase):
+    def test_field_wrapper_not_used(self):
+        def make_serializer():
+            class TestSerializer(Serializer):
+                status = Integer()
+
+        self.assertRaises(ConfigurationError, make_serializer)
