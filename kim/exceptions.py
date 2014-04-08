43566d617855c46c680c7bf447cf9d4249979970
Mike Waites <mikey.waites@gmail.com>
2014-04-08 16:27:31 +0100

    Refactoring exceptions, added MappingErrors and FieldError

--- kim/exceptions.py
+++ kim/exceptions.py
@@ -4,25 +4,36 @@ class KimError(Exception):
     pass
 
 
-class MappingError(KimError):
-    pass
-
-
-#TODO REMOVE THIS
 class ValidationError(KimError):
+    """Exception used by Types to raise an error when their validate
+    method fails
+
+    ..seealso::
+        :meth:`kim.types.BaseType.validate`
+
+    """
     pass
 
 
-class ValidationErrors(KimError):
+class FieldError(ValidationError):
+    """A type of :class:`.ValidationError` that also accepts the field
+    name specified as `key`
+
+    """
+
+    def __init__(self, key, *args, **kwargs):
+        super(FieldError, self).__init__(*args, **kwargs)
+
+        self.key = key
+
+
+class MappingErrors(KimError):
     pass
 
 
 class RoleNotFound(KimError):
     pass
 
-
-class FieldError(KimError):
-    pass
 
 class ConfigurationError(KimError):
     pass
