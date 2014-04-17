899545487c2e5951c9f827d512f41e534f740cef
Jack Saunders <jack@oldstlabs.com>
2014-04-17 08:24:54 -0700

    add deps to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,11 @@ Links
 from __future__ import print_function
 from setuptools import setup
 
+install_requires = [
+    'SQLAlchemy',
+    'iso8601',
+]
+
 setup(
     name='Kim',
     version='0.0.1',
@@ -23,11 +28,11 @@ setup(
     author_email='mikey.wait.es@gmail.com',
     description='A pyhton serialization library',
     long_description=__doc__,
-    packages=['kim', 'kim.tests'],
+    packages=['kim'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=[],
+    install_requires=install_requires,
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
