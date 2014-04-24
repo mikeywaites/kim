38db93085532051ec1422b944fa4a113a4c4534b
Mike Waites <mikey.waites@gmail.com>
2014-04-24 12:08:48 +0100

    Added tests for NumericType

--- kim/tests/test_types.py
+++ kim/tests/test_types.py
@@ -174,6 +174,44 @@ class PositiveIntegerTypeTests(unittest.TestCase):
         my_type.validate(1)
 
 
+class NumericTypeTests(unittest.TestCase):
+
+    def test_numeric_string_is_accepted(self):
+
+        my_type = NumericType()
+        self.assertTrue(my_type.validate('1'))
+
+    def test_int_is_accpepted(self):
+
+        my_type = NumericType()
+        self.assertTrue(my_type.validate(1))
+
+    def test_validation_error_raised_with_non_numeric_string(self):
+
+        my_type = NumericType()
+        with self.assertRaises(ValidationError):
+            self.assertTrue(my_type.validate('woops'))
+
+    def test_validate_choice_with_valid_choice(self):
+
+        class MyType(TypedType):
+
+            type_ = str
+
+        my_type = MyType(choices=['1', 1])
+        self.assertTrue(my_type.validate('1'))
+
+    def test_validate_choice_with_invalid_choice(self):
+
+        class MyType(NumericType):
+
+            type_ = str
+
+        my_type = MyType(choices=['1', 'foo'])
+        with self.assertRaises(ValidationError):
+            self.assertTrue(my_type.validate('foo'))
+
+
 class CollectionTypeTests(unittest.TestCase):
 
     def test_marshal_value(self):
