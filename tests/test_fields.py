ff4b0a2b22aeacb4de38280eaef87ea352501b10
Mike Waites <mikey.waites@gmail.com>
2014-05-29 21:23:49 +0100

    more tests for Field api.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import mock
 
 from kim.fields import Field
+from kim.types import String
+from kim.exceptions import ValidationError
 
 
 class MyType(object):
@@ -76,9 +79,86 @@ class FieldTests(unittest.TestCase):
         self.assertEqual(field.source, 'name')
         self.assertEqual(field._source, None)
 
-    def test_srt_field_id_param(self):
+    def test_set_field_id_param(self):
         """ensure that field_id is correctly set when passed as a kwarg
         """
         field = Field('name', MyType(), field_id='other_name')
         self.assertEqual(field.field_id, 'other_name')
         self.assertEqual(field._field_id, 'other_name')
+
+    def test_field_id_returns_name_when_not_set(self):
+        """ensure that field_id returns ``Field.name`` when ``Field._field_id``
+        is None
+        """
+        field = Field('name', MyType())
+        self.assertEqual(field.field_id, 'name')
+        self.assertEqual(field._field_id, None)
+
+    def test_read_only_field_is_valid_when_required_value_is_none(self):
+        """ensure a read only field is always considered valid
+        """
+        field = Field('name', String(), required=True, read_only=True)
+        self.assertTrue(field.is_valid(None))
+
+    def test_required_field_raises_error_when_value_is_none(self):
+
+        field = Field('name', String())
+        with self.assertRaises(ValidationError):
+            field.is_valid(None)
+
+    def test_required_validation_error_message(self):
+
+        field = Field('name', String())
+        try:
+            field.is_valid(None)
+        except ValidationError as e:
+            self.assertEqual(e.message, 'This is a required field')
+        else:
+            assert False is True, 'Field is not valid'
+
+    def test_non_required_field_and_allow_none(self):
+        """ensure that a non required field that does not allow null values
+        raises an error when validating None
+        """
+
+        field = Field('name', String(), required=False, allow_none=False)
+        with self.assertRaises(ValidationError):
+            field.is_valid(None)
+
+    def test_field_allow_none_error_message(self):
+        field = Field('name', String(), required=False, allow_none=False)
+        try:
+            field.is_valid(None)
+        except ValidationError as e:
+            self.assertEqual(e.message, 'This field can not be None')
+        else:
+            assert False is True, 'Field is not valid'
+
+    def test_field_type_validate_method_called(self):
+        """ensure that after a field has run validation against required etc,
+        it calls the validate method of the provided field type
+        """
+        mocked = mock.MagicMock()
+
+        class MyValidateType(String):
+
+            def validate(self, value):
+                mocked()
+                return super(MyValidateType, self).validate(value)
+
+        field = Field('name', MyValidateType())
+        field.is_valid('string')
+        self.assertTrue(mocked.called)
+
+    def test_extra_validators_run(self):
+
+        mock_validator_1 = mock.MagicMock()
+        mock_validator_2 = mock.MagicMock()
+        mock_validator_3 = mock.MagicMock()
+        validators = [mock_validator_1, mock_validator_2, mock_validator_3]
+
+        field = Field('name', String(), extra_validators=validators)
+        field.is_valid('string')
+        self.assertTrue(mock_validator_1.called)
+        self.assertTrue(mock_validator_2.called)
+        self.assertTrue(mock_validator_3.called)
