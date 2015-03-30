5d09ea50d050e2a62e64e9edbe40b02f5b84a01f
Mike Waites <mikey.waites@gmail.com>
2015-03-30 19:46:46 +0100

    added tests for mapper instantiation.

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,9 +1,58 @@
+import six
+
+from collections import OrderedDict
+
 from kim.mapper import Mapper
+from kim.fields import Field
 
 
-def test_mapper_instantiation():
-    """Simple test that instantiates a new ``Mapper`` object as a proof of
-    concept for python 2.x and 3.x support.
+class TestField(Field):
+    pass
+
+
+def test_mapper_sets_declared_fields():
+    """Ensure that on attributes inheriting from :class:`kim.fields.Field`
+    are set in a mappers fields.
     """
+
+    class MyTestMapper(Mapper):
+
+        name = TestField()
+        other = 'not a field'
+
     mapper = Mapper()
-    assert isinstance(mapper, Mapper)
+    mapper_with_fields = MyTestMapper()
+    assert mapper.declared_fields == OrderedDict()
+    assert 'name' in mapper_with_fields.declared_fields
+    assert isinstance(mapper_with_fields.declared_fields['name'], TestField)
+    assert 'other' not in mapper_with_fields.declared_fields
+
+
+def test_mapper_inheritance():
+
+    class OtherField(Field):
+        pass
+
+    class MapperBase(Mapper):
+
+        id = TestField()
+        name = TestField()
+
+    class NewMapper(MapperBase):
+
+        id = OtherField()
+        additional_field = TestField()
+
+    mapper = MapperBase()
+    other_mapper = NewMapper()
+
+    assert len(mapper.declared_fields.keys()) == 2
+    assert 'id' in mapper.declared_fields
+    assert 'name' in mapper.declared_fields
+
+    assert len(other_mapper.declared_fields.keys()) == 3
+    assert 'id' in other_mapper.declared_fields
+    assert 'name' in other_mapper.declared_fields
+    assert 'additional_field' in other_mapper.declared_fields
+
+    assert isinstance(other_mapper.declared_fields['id'], OtherField)
