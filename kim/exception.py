b65004642205e12a4cb3b9ab34defcb5556e8f64
Mike Waites <mikey.waites@gmail.com>
2015-04-12 22:13:35 +0100

    added exception module

---
+++ kim/exception.py
@@ -0,0 +1,10 @@
+# kim/exception.py
+# Copyright (C) 2014-2015 the Kim authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of Kim and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+
+class MapperError(Exception):
+    pass
