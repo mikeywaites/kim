4bfd55802ac1211b08f9165000dc5243685af601
Mike Waites <mikey.waites@gmail.com>
2015-05-08 17:44:14 +0100

    added whitelist/blacklist role types with membership overloading

--- kim/role.py
+++ kim/role.py
@@ -9,13 +9,35 @@
 class Role(set):
 
     def __init__(self, *args, **kwargs):
-        super(Role, self).__init__(args, **kwargs)
+        self.whitelist = kwargs.pop('whitelist', True)
+        super(Role, self).__init__(args)
 
     @property
     def fields(self):
 
         return [k for k in self]
 
+    def __contains__(self, key):
+        if self.whitelist:
+            return super(Role, self).__contains__(key)
+        else:
+            return not super(Role, self).__contains__(key)
+
     def __or__(self, other):
         result = super(Role, self).__or__(other)
         return Role(*[k for k in result])
+
+
+class whitelist(Role):
+
+    def __init__(self, *args, **kwargs):
+        self.whitelist = True
+        kwargs['whitelist'] = True
+        super(whitelist, self).__init__(*args, **kwargs)
+
+
+class blacklist(Role):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['whitelist'] = False
+        super(blacklist, self).__init__(*args, **kwargs)
--- tests/test_role.py
+++ tests/test_role.py
@@ -1,4 +1,20 @@
-from kim.role import Role
+from kim.role import Role, whitelist, blacklist
+
+
+def test_whitelist_membership():
+
+    role = whitelist('name', 'id')
+    import ipdb; ipdb.set_trace()
+    assert 'name' in role
+    assert 'id' in role
+    assert 'email' not in role
+
+
+def test_blacklist_membership():
+
+    role = blacklist('name', 'id')
+    res = 'name' in role
+    assert res is False
 
 
 def test_merge_roles():
