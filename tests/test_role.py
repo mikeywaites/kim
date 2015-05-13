0608abf7bd7ac92f772333bcea03095286fe231c
Mike Waites <mikey.waites@gmail.com>
2015-05-13 21:07:33 +0100

    Handle unions between whitelist/blacklist Role types

--- tests/test_role.py
+++ tests/test_role.py
@@ -16,9 +16,39 @@ def test_blacklist_membership():
     assert res is False
 
 
-def test_merge_roles():
+def test_union_whitelist_roles():
 
-    role = Role('name', 'id') | Role('name', 'email')
+    role = whitelist('name', 'id') | whitelist('name', 'email')
     assert 'name' in role
     assert 'id' in role
     assert 'email' in role
+    assert role.whitelist
+
+
+def test_union_whitelist_and_blacklist():
+
+    role = whitelist('name', 'id') | blacklist('name', 'email')
+    assert 'id' in role
+    assert 'name' not in role
+    assert 'email' not in role
+    assert role.whitelist
+
+
+def test_union_blacklist_and_whitelist():
+
+    role = blacklist('name', 'id') | whitelist('name', 'email')
+    assert 'email' in role
+    assert 'name' not in role
+    assert 'id' not in role
+    assert role.whitelist
+
+
+def test_union_blacklist_and_blacklist():
+
+    role = blacklist('name', 'id') | blacklist('name', 'email')
+    # use fields here to test for membership as it's a blacklist so membership
+    # is negated
+    assert 'name' in role.fields
+    assert 'id' in role.fields
+    assert 'email' in role.fields
+    assert role.whitelist is False
