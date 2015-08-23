f46b5e01b699b82fe74de209080c8a59435911e8
Jack Saunders <jack@oldstlabs.com>
2015-08-23 18:49:36 +0100

    tests for SQA nested fields

--- tests/test_sqa.py
+++ tests/test_sqa.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from kim.mapper import Mapper
 from kim import field
+from kim.field import FieldInvalid
 
 
 Base = declarative_base()
@@ -53,7 +54,7 @@ def db_session(request, connection):
     return DBSession()
 
 
-def test_marshal_nested_mapper(db_session):
+def test_marshal_nested_mapper_allow_create(db_session):
 
     class UserMapper(Mapper):
 
@@ -83,3 +84,157 @@ def test_marshal_nested_mapper(db_session):
     assert isinstance(obj, Post)
     assert obj.title == 'my post'
     assert isinstance(obj.user, User)
+
+
+def test_marshal_nested_mapper_defaults(db_session):
+
+    class UserMapper(Mapper):
+
+        __type__ = User
+
+        id = field.Integer(read_only=True)
+        name = field.String()
+
+    def getter(field, data):
+        return db_session.query(User).get(data['id'])
+
+    class PostMapper(Mapper):
+
+        __type__ = Post
+
+        title = field.String()
+        user = field.Nested('UserMapper', required=True, getter=getter)
+
+    data = {
+        'id': 2,
+        'title': 'my post',
+        'user': {
+            'id': 1,
+            'name': 'should be ignored',
+        }
+    }
+    user = User(id=1, name='mike')
+    instance = Post(title='my post', user=user)
+
+    db_session.add(instance)
+    db_session.flush()
+
+    mapper = PostMapper(data=data, obj=instance)
+    obj = mapper.marshal()
+
+    assert obj.user == user
+    assert obj.user.name == 'mike'
+
+
+def test_marshal_nested_mapper_defaults_not_found(db_session):
+
+    class UserMapper(Mapper):
+
+        __type__ = User
+
+        id = field.Integer(read_only=True)
+        name = field.String()
+
+    def getter(field, data):
+        return db_session.query(User).get(data['id'])
+
+    class PostMapper(Mapper):
+
+        __type__ = Post
+
+        title = field.String()
+        user = field.Nested('UserMapper', required=True, getter=getter)
+
+    data = {
+        'id': 2,
+        'title': 'my post',
+        'user': {
+            'id': 1,
+            'name': 'should be ignored',
+        }
+    }
+
+    mapper = PostMapper(data=data)
+
+    with pytest.raises(FieldInvalid):
+        mapper.marshal()
+
+
+def test_marshal_nested_mapper_allow_updates(db_session):
+
+    class UserMapper(Mapper):
+
+        __type__ = User
+
+        id = field.Integer(read_only=True)
+        name = field.String()
+
+    def getter(field, data):
+        return db_session.query(User).get(data['id'])
+
+    class PostMapper(Mapper):
+
+        __type__ = Post
+
+        title = field.String()
+        user = field.Nested('UserMapper', required=True, getter=getter,
+                            allow_updates=True)
+
+    data = {
+        'id': 2,
+        'title': 'my post',
+        'user': {
+            'id': 1,
+            'name': 'new name',
+        }
+    }
+    user = User(id=1, name='mike')
+    instance = Post(title='my post', user=user)
+
+    db_session.add(instance)
+    db_session.flush()
+
+    mapper = PostMapper(data=data, obj=instance)
+    obj = mapper.marshal()
+
+    assert obj.user == user
+    assert obj.user.name == 'new name'
+
+
+def test_marshal_nested_mapper_allow_updates_in_place(db_session):
+
+    class UserMapper(Mapper):
+
+        __type__ = User
+
+        name = field.String()
+
+    def getter(field, data):
+        return db_session.query(User).get(data['id'])
+
+    class PostMapper(Mapper):
+
+        __type__ = Post
+
+        title = field.String()
+        user = field.Nested('UserMapper', required=True,
+                            allow_updates_in_place=True)
+
+    data = {
+        'id': 2,
+        'title': 'my post',
+        'user': {
+            'name': 'new name',
+        }
+    }
+    user = User(id=1, name='mike')
+    instance = Post(title='my post', user=user)
+
+    db_session.add(instance)
+    db_session.flush()
+
+    mapper = PostMapper(data=data, obj=instance)
+    obj = mapper.marshal()
+
+    assert obj.user == user
+    assert obj.user.name == 'new name'
