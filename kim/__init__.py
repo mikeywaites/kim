934186858c3bf2ec538d3de582a39fb2cbc7251e
Jack Saunders <jackqu7@gmail.comm>
2017-03-26 08:33:17 +0100

    put everything in __init__ and define __all__

--- kim/__init__.py
+++ kim/__init__.py
@@ -9,5 +9,19 @@
 __version__ = '1.0.0'
 
 
-from .mapper import Mapper
-from .field import Field
+from .mapper import Mapper, PolymorphicMapper
+from .exception import (
+    MapperError, MappingInvalid, RoleError, FieldOptsError, FieldError,
+    FieldInvalid, StopPipelineExecution)
+from .role import blacklist, whitelist
+from .pipelines import pipe
+from .field import (
+    Field, String, Integer, Decimal, Boolean, Nested, Collection, Static,
+    DateTime, Date)
+
+
+__all__ = [
+    Mapper, PolymorphicMapper, MapperError, MappingInvalid, RoleError,
+    FieldOptsError, FieldError, FieldInvalid, StopPipelineExecution, blacklist,
+    whitelist, pipe, Field, String, Integer, Decimal, Boolean, Nested,
+    Collection, Static, DateTime, Date]
